5f9f62424c52a98f679811293668531eec9c197c
Briso10-dev <kouambrice10@gmail.com>
2025-04-19 13:56:19 +0200

    Packing list component in progress

--- src/components/packing-list-.jsx
+++ src/components/packing-list-.jsx
@@ -1,6 +1,11 @@
 
 
 export default function PackingList(){
-    
+    return(
+        <div className=" bg-[#5a3e2b] text-[#ffebb3] py-4 flex flex-col justify-between items-center gap-4">
+            LIST
+        </div>
+
+    )
 
 }
