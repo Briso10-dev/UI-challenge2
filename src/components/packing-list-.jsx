87f747aec1eed51d33b2722490330ea02bd4c9d1
Briso10-dev <kouambrice10@gmail.com>
2025-04-19 13:58:46 +0200

    Stats components in progress

--- src/components/packing-list-.jsx
+++ src/components/packing-list-.jsx
@@ -7,5 +7,4 @@ export default function PackingList(){
         </div>
 
     )
-
 }
--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -1,6 +1,10 @@
 
 
 export default function Stats(){
-    
+    return(
+        <footer>
+            You have X items on your list, and you already packed X (X%)
+        </footer>
+    )
 
 }
