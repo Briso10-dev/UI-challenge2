c9fc146e8300e529cbc4889fe1f3d40248459a40
Briso10-dev <kouambrice10@gmail.com>
2025-04-23 21:44:13 +0200

    Adding a styling condition for true items list

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -1,7 +1,7 @@
 export default function Item({item}){
     return (
         <>
-        <span>
+        <span style={item.packed ? {textDecoration: "line-through" } : {}}>
             {item.quantity}
             {item.description}
         </span>
