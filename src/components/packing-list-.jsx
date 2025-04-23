473a573f907683d8e0d58edac382f780d45cd22e
Briso10-dev <kouambrice10@gmail.com>
2025-04-23 20:55:51 +0200

    Item component done and in connection with the package list component

--- src/components/packing-list-.jsx
+++ src/components/packing-list-.jsx
@@ -1,14 +1,17 @@
+import Item from "./item";
 
-const initialItems = [
+export const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
 ];
 
 
-export default function PackingList(){
+export function PackingList(){
     return(
         <ul className=" bg-[#5a3e2b] text-[#ffebb3] text-[1.4rem] py-4 flex flex-col justify-between items-center gap-4">
-            {initialItems.map(items=><Item item={item} />)}
+            {initialItems.map((item) => (
+                <Item item={item} />
+            ))}
         </ul>
 
     )
