f0aa7766578b834495fcdcc48e7b2027a9c51142
Briso10-dev <kouambrice10@gmail.com>
2025-04-23 21:41:00 +0200

    Adding another object in item list

--- src/components/packing-list.jsx
+++ src/components/packing-list.jsx
@@ -3,6 +3,7 @@ import Item from "./item";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Charger", quantity: 1, packed: false },
 ];
 
 
