99edc901fcc46d42d622ff62c156b0bc7ea9a3c5
Briso10-dev <kouambrice10@gmail.com>
2025-04-24 05:49:30 +0200

    Resettling footer height size

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -2,7 +2,7 @@
 
 export default function Stats(){
     return(
-        <footer className="bg-[#76c7ad] text-2xl text-center font-semibold">
+        <footer className="bg-[#76c7ad] text-3xl text-center font-semibold  h-[6.2rem] py-[3.2rem]">
            <em> You have X items on your list, and you already packed X (X%)</em>
         </footer>
     )
