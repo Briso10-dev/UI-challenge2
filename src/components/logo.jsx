c2454397da58fd8afc266faaa8d233fa00afa217
Briso10-dev <kouambrice10@gmail.com>
2025-04-24 05:42:19 +0200

    Limiting space between the emojis and logo title

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -2,7 +2,7 @@
 
 export default function Logo(){
     return(
-        <h1 className="text-center bg-[#f4a226] font-monoton text-[5rem] uppercase font-normal tracking-[3rem] py-[2rem]">🌴 Far away 👜</h1>
+        <h1 className="text-center bg-[#f4a226] font-monoton text-[5rem] uppercase font-normal tracking-[3rem] py-[2rem]">🌴Far away👜</h1>
     )
 
 }
