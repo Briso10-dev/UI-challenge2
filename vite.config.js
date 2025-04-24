3783c5c4cd8d7e55235c0554f4f732000007f69c
Briso10-dev <kouambrice10@gmail.com>
2025-04-24 06:03:28 +0200

    Resetiting vite.config.js as before and importing its changes to tailwind.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  theme: {
-    extend: {
-      fontFamily: {
-        monoton: ['"Monoton"', 'cursive'],
-      },
-    },
-  },
-
   plugins: [
     react(),
     tailwindcss()
