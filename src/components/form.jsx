f5ca670f17d963e678a9f4bf6ce6820a2b22784f
Briso10-dev <kouambrice10@gmail.com>
2025-04-24 06:46:11 +0200

    An array in form for stocking the values

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -5,12 +5,15 @@ export default function Form(){
         <form className="bg-[#e5771f] py-[3rem] flex items-center justify-center gap-[1rem] text-[2.4rem] h-[6.8rem]">
             <h3>What do you need for your 😍 trip?</h3>
             <select name="" id="">
-                <option value="{1}">1</option>
-                <option value="{2}">2</option>
-                <option value="{3}">3</option>
-            </select>
+                {Array.from({ length: 20}, (_, i) => i + 1).map
+                ((num) => (
+                    <option value ={num} key={num}> 
+                        {num} 
+                    </option> 
+                ))}
+            </select> 
             <input type="text" placeholder="Item..." />
-            <button>Add</button>
+            <button className="uppercase bg-[#76c7ad]">Add</button>
         </form>
     )
 }
