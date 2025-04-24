d0a14eea60fd2f7fba8d9e6fee72dc9b2634822f
Briso10-dev <kouambrice10@gmail.com>
2025-04-24 12:20:23 +0200

    Controlling element for quantity in the form component

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 
 export default function Form(){
     cont [description,setDescription] = useState("TEST");
+    const [quantity, setQuantity] = useState(5);
+
 
     function handleSubmit(e){
         e.preventDefault();
@@ -11,7 +13,7 @@ export default function Form(){
     return(
         <form onSubmit={handleSubmit} className="bg-[#e5771f] py-[3rem] flex items-center justify-center gap-[1rem] text-[2.4rem] h-[6.8rem]">
             <h3>What do you need for your 😍 trip?</h3>
-            <select name="" id="">
+            <select name="" id="" value ={quantity} onChange={(e)=> setQuantity(e.target.value)}>
                 {Array.from({ length: 20}, (_, i) => i + 1).map
                 ((num) => (
                     <option value ={num} key={num}> 
