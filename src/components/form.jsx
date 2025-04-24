964adf0d17973bdb439927ecf64e89a2ee2dce47
Briso10-dev <kouambrice10@gmail.com>
2025-04-24 05:41:00 +0200

    Arranging form's component height size

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -2,7 +2,7 @@
 
 export default function Form(){
     return(
-        <div className="bg-[#e5771f] py-[2rem] flex items-center justify-center gap-[0.8rem] text-[2.2rem]">
+        <div className="bg-[#e5771f] py-[3rem] flex items-center justify-center gap-[1rem] text-[2.4rem] h-[6.8rem]">
             <h3>What do you need for your 😍 trip?</h3>
         </div>
     )
