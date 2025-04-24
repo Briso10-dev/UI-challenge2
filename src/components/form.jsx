b151de348b17d3c1526a5ab0a0222b2f52a34532
Briso10-dev <kouambrice10@gmail.com>
2025-04-24 06:29:08 +0200

    Setting items list horizontally

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -2,8 +2,15 @@
 
 export default function Form(){
     return(
-        <div className="bg-[#e5771f] py-[3rem] flex items-center justify-center gap-[1rem] text-[2.4rem] h-[6.8rem]">
+        <form className="bg-[#e5771f] py-[3rem] flex items-center justify-center gap-[1rem] text-[2.4rem] h-[6.8rem]">
             <h3>What do you need for your 😍 trip?</h3>
-        </div>
+            <select name="" id="">
+                <option value="{1}">1</option>
+                <option value="{2}">2</option>
+                <option value="{3}">3</option>
+            </select>
+            <input type="text" placeholder="Item..." />
+            <button>Add</button>
+        </form>
     )
 }
